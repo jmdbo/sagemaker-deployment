aaf50758ddb2bd6a83659b570d451131cf21ed0b
João Barata Oliveira <joao@joaobarata.eu>
2021-03-21 01:45:22 +0000

    Project Elaboration
    Added all the required features to the final project.

--- Project/train/train.py
+++ Project/train/train.py
@@ -66,8 +66,26 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     loss_fn      - The loss function used for training.
     device       - Where the model and data should be loaded (gpu or cpu).
     """
-    
-    # TODO: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            #Reset gradients to zero in optimizer
+            optimizer.zero_grad()
+            # Forward pass
+            output = model(batch_X)
+            #Get losses
+            loss = loss_fn(output, batch_y)
+            #Backward pass
+            loss.backward()
+            #Perform optimization step
+            optimizer.step()            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
     pass
 
